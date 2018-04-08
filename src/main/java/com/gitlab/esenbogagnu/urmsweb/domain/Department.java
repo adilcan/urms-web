6340388be81fdf65f1f6f2de12b976d22a347245
Adilcan Eren <adilcan@kodgemisi.com>
2018-04-08 17:39:38 +0300

    adds auditing

--- src/main/java/com/gitlab/esenbogagnu/urmsweb/domain/Department.java
+++ src/main/java/com/gitlab/esenbogagnu/urmsweb/domain/Department.java
@@ -1,12 +1,20 @@
 package com.gitlab.esenbogagnu.urmsweb.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -20,6 +28,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Department extends BaseEntity {
 
 	@NotEmpty
@@ -34,4 +43,19 @@ public class Department extends BaseEntity {
 
 	@ManyToMany
 	private List<Risk> risks;
+
+	@CreatedDate
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:SS")
+	private LocalDate creationDate;
+
+	@CreatedBy
+	private String createdBy;
+
+	@LastModifiedDate
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:SS")
+	private LocalDate modifyDate;
+
+	@LastModifiedBy
+	private String modifiedBy;
+
 }
