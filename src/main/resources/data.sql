a696883b3b034afc95794b1f3087275bd1967800
Adilcan Eren <adilcan@kodgemisi.com>
2018-04-11 22:26:21 +0300

    adds data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO deparments(id, created_by, creation_date, email, name) VALUES
+    (0, 'admin', '2018-04-11', 'urmshealth@mailinator.com', 'Health');
+INSERT INTO deparments(id, created_by, creation_date, email, name) VALUES
+    (0, 'admin', '2018-04-11', 'urmshr@mailinator.com', 'Human Resources');
+INSERT INTO deparments(id, created_by, creation_date, email, name) VALUES
+    (0, 'admin', '2018-04-11', 'urmsfinance@mailinator.com', 'Finance');
